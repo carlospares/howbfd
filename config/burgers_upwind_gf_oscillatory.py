f81ac459c5fb0fb904a135926bde901c9689d292
KAZOLEA Maria <maria.kazolea@inria.fr>
2024-01-16 15:33:51 +0100

    config for oscillations case

---
+++ config/burgers_upwind_gf_oscillatory.py
@@ -0,0 +1,31 @@
+from initcond import InitCond
+from equation import Equation
+from functionH import FunH
+from eq_sw import SWEquation
+from boundary import BoundaryCond
+from nummeth import NumericalMethod
+from timest import TimeStepping
+
+# For a detailed explanation, see howbfd_config
+
+equation = Equation.BURGERS
+init = InitCond.STEADY
+funh = FunH.IDpSIN#IDENT#IDpSIN
+H_noise_factor = 0.0
+perturb_init = InitCond.PERT_NONE#GAUSS
+boundary = BoundaryCond.FORCE_STEADY_INIT
+nummeth = NumericalMethod.UPWINDGF
+timest = TimeStepping.TVDRK3
+order = 9
+N = 30
+cfl = 0.5
+a = -1
+b = 1
+T = 1.0#0.0037157669102204603
+plot_every = 2.1
+show_plots = True
+save_plots = False
+save_npys = False
+plot_exact = True
+
+print "Loaded config/burgers_upwind_gf.py!"
