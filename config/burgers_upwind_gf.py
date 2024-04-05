c907dfb9e8e181cbb66780231b33d7cad45b18a5
RICCHIUTO Mario <mario.ricchiuto@inria.fr>
2024-04-05 11:00:25 +0200

    config with output printing

--- config/burgers_upwind_gf.py
+++ config/burgers_upwind_gf.py
@@ -14,18 +14,18 @@ funh = FunH.DISC#IDENT
 H_noise_factor = 0.0
 perturb_init = InitCond.PERT_GAUSS#NONE
 boundary = BoundaryCond.FORCE_STEADY_INIT
-nummeth = NumericalMethod.UPWINDGF#WB#GF
+nummeth = NumericalMethod.UPWINDWB#GF
 timest = TimeStepping.TVDRK3
-order = 5 
+order = 7 
 N = 20
 cfl = 0.75
 a = -1
 b = 1
-T = 0.7#0.0037157669102204603
-plot_every = 0.1 
-show_plots = True
+T = 0.5#0.0037157669102204603
+plot_every = 0.3 
+show_plots = False 
 save_plots = False
-save_npys = False
-plot_exact = True
+save_npys = True
+plot_exact = False 
 
 print "Loaded config/burgers_upwind_gf.py!"
