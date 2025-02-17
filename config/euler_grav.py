bcaa820f550db66db1e133f6d595d9fb709ec169
Maria Kazolea <maria.kazolea@inria.fr>
2025-02-17 15:13:02 +0100

    config for euler

---
+++ config/euler_grav.py
@@ -0,0 +1,37 @@
+from initcond import InitCond
+from equation import Equation
+from functionH import FunH
+from eq_sw import SWEquation
+from boundary import BoundaryCond
+from nummeth import NumericalMethod
+from timest import TimeStepping
+
+# For a detailed explanation, see howbfd_config
+
+equation = Equation.EulerGRAV
+init = InitCond.Eulergrav#DISCRETE_AB#STEADY#READ_FROM_FILE#STEADY #WATER_AT_REST
+funh = FunH.EUL_GRAV
+H_noise_factor = 0
+perturb_init = InitCond.PERT_NONE#DISC#PERT_RIEMANN#DISC#PERT_WB #PERT_NONE
+boundary = BoundaryCond.IN_OUT #FORCE_STEADY_INIT
+nummeth = NumericalMethod.UPWINDGF#RUSANOV#WB #UPWINDGF
+timest = TimeStepping.TVDRK3#EULER#TVDRK3
+order = 5
+N = 25
+a = 0
+b = 1
+cfl = 0.4
+T = 0.1644
+steps=4
+ode='AB'
+system='SW'
+compute_source='analytic_source'#'hydrostatic_reconstruction'#'source_reconstruction'#'analytic_source'#'hydrostatic_reconstruction' 
+plot_exact = False
+plot_every = 15.0
+show_plots = True
+save_plots = False
+save_npys = False
+
+
+
+print ("Loaded config/euler_grav.py!")
