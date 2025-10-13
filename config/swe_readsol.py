dbba9382f90264e7d3a319474561c214e8b7f0bc
KAZOLEA Maria <your_registered_email@example.com>
2025-10-13 19:08:57 +0200

    config swe

--- config/swe_readsol.py
+++ config/swe_readsol.py
@@ -10,25 +10,25 @@ from timest import TimeStepping
 
 equation = Equation.SW
 init = InitCond.WATER_AT_REST#DISCRETE_AB#STEADY#READ_FROM_FILE#STEADY #WATER_AT_REST
-funh = FunH.BUMPS#STEP#BUMP2#D# BUMPS
+funh = FunH.STEP#BUMPS#STEP#BUMP2#D# BUMPS
 H_noise_factor = 0
-perturb_init = InitCond.PERT_GAUSS#DISC#PERT_RIEMANN#DISC#PERT_WB #PERT_NONE
+perturb_init = InitCond.PERT_NONE#DISC#GAUSS#DISC#PERT_RIEMANN#DISC#PERT_WB #PERT_NONE
 boundary = BoundaryCond.WALL#FORCE_STEADY_INIT#FORCE_DISCRETE_STEADY_INIT#SUPER_RE#FORCE_STEADY_INIT#FORCE_DISCRETE_STEADY_INIT#SUBCR#SUPER#FORCE_DISCRETE_STEADY_INIT#SUPER#_RE#SUBCR#SUBCR_RE#IN_OUT #FORCE_STEADY_INIT
 nummeth = NumericalMethod.UPWINDNCGF#RUSANOVGWB #UPWINDGF
 timest = TimeStepping.TVDRK3
-order = 3
+order = 7
 N = 25
 a = 0#-3 #0
 b = 25#3 #25
 cfl = 0.4
-T = 0.25
+T = 1.0
 steps=4
 ode='AB'
 system='SW'
 compute_source='analytic_source'#'source_reconstruction'#'analytic_source'#'hydrostatic_reconstruction' 
 plot_exact = False
-plot_every = 1.1
-show_plots = True
+plot_every = 1.5
+show_plots = False
 save_plots = False
 save_npys = False
 
